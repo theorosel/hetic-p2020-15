a9906d4a124505e82778547a18004b58ee0251b7
theorosel <theo.rosel@hetic.net>
2017-11-02 14:06:51 +0100

    :construction: Work on hero board

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,10 @@ class Hero {
         this.$el.cursorDot       = this.$el.container.querySelector('.dot__start--blue')
         this.$el.strokeCircle    = this.$el.container.querySelector('.progress')
         this.$el.strokeProgress  = this.$el.container.querySelector('.progress__value')
-        // this.$el.board           = document.querySelector('.board__img')
-        // this.$el.shadow          = document.querySelector('.shadow')
+        this.$el.boardContainer  = this.$el.container.querySelector('.board__container')
+        this.$el.board           = this.$el.container.querySelector('.board__img')
+        this.rotationCoef        = 20
+        this.$el.shadow          = this.$el.container.querySelector('.shadow')
 
         this.cursor         = new Hammer(this.$el.cursor)
         this.cursorPosition = 0
@@ -35,9 +37,10 @@ class Hero {
      * and place event listeners
      */
     init() {
-
+        console.log(this.$el.container,this.$el.height, this.$el.board);
         this.cursor.on('panstart', (event) => {
             this.cursor.obj.active = true
+            this.$el.board.classList.remove('levitate')
         })
 
         this.cursor.on('panmove', (event) => {
@@ -74,6 +77,7 @@ class Hero {
 
         this.cursor.on('panend', (event) => {
             let middle = Math.round(this.$el.line.getBoundingClientRect().width / 2)
+            this.$el.board.classList.add('levitate')
 
             if (this.temp_move >= 0 && this.temp_move >= middle) {
 
@@ -109,26 +113,68 @@ class Hero {
             }
         })
 
+        if (window.matchMedia("(min-width: 600px)").matches) {
+
+            console.log('desktop');
+            this.$el.container.addEventListener('mousemove', (event) => {
+                let x = event.clientX
+                let y = event.clientY
+                let mapX = map(x, 0, this.$el.width, -1, 1)
+                let mapY = map(y, 0, this.$el.height, -1, 1)
+
+                // console.log(mapX, mapY);
+
+                new TweenMax(this.$el.boardContainer, 0.9, {
+                    x: mapX * this.rotationCoef,
+                    y: mapY * this.rotationCoef,
+                    // rotationX: mapX * this.rotationCoef,
+                    // rotationY: mapY * this.rotationCoef,
+                    rotationZ: 0,
+                    ease: Power0.easeOut
+                })
+            })
+        }
+        else {
+
+            console.log('mobile');
+            this.$el.container.addEventListener('devicemotion', (event) => {
+                // ... Do something on mobile using gyroscope
+                console.log(event)
+                console.log('yo');
+            })
+        }
+
         window.addEventListener('resize', () => {
             this.resize()
         })
     }
 
+
+    /*
+     * updateStroke(Value : float)
+     * Fill the stroke circle according to drag value
+     * and place event listeners
+     */
     updateStroke(value) {
         let dashoffset = this.strokeCircum * (1 - value);
-        // console.log('progress:', value + '%', '|', 'offset:', dashoffset)
 
         this.$el.strokeProgress.style.strokeDashoffset = dashoffset;
         this.$el.strokeProgress.style.strokeDasharray = this.strokeCircum;
+        // console.log('progress:', value + '%', '|', 'offset:', dashoffset)
     }
 
+
+    /*
+     * resize()
+     * Fill the stroke circle according to drag value
+     * and place event listeners
+     */
     resize() {
         this.cursor.obj.leftLimit = Math.round(this.$el.line.getBoundingClientRect().left),
         this.cursor.obj.rightLimit = Math.round(this.$el.line.getBoundingClientRect().left + this.$el.line.getBoundingClientRect().width),
         this.cursorPosition = Math.round(this.$el.cursor.getBoundingClientRect().left) + (this.$el.cursor.offsetWidth / 2)
-        console.log(this.cursor.obj.leftLimit, this.cursor.obj.rightLimit);
     }
 }
 
-const hero = new Hero(document.querySelector('.control'))
+const hero = new Hero(document.querySelector('.hero'))
 hero.init()
