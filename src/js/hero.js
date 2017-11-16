dcb55bf094c4f02c2688dc1c43f4b01134b718e4
theorosel <theo.rosel@hetic.net>
2017-11-16 02:38:16 +0100

    :sparkles: js > hero.js > Add drag control intro

--- src/js/hero.js
+++ src/js/hero.js
@@ -3,9 +3,6 @@ import Preloader from './components/Preloader'
 import Loader from './components/Loader'
 
 const preloader = new Preloader()
-preloader.on('progress', value => {
-    console.log(value)
-})
 
 preloader.on('complete', () => {
     const loader = new Loader()
@@ -23,10 +20,12 @@ preloader.on('complete', () => {
         setTimeout(() => {
             const $board = document.querySelector('.board__img')
             const $shadow = document.querySelector('.board__shadow')
+            const $dragCursor = document.querySelector('.cursor')
+            const $dragCursorDot = document.querySelectorAll('.cursor__dot')
+            const $dragLine = document.querySelector('.control__line')
+            const $dragCircle = document.querySelector('.progress__meter')
             const timeline = new TimelineMax()
-            console.log($board)
-    
-    
+
             timeline
             .staggerTo($loaderParts, 0.9, {
                 scaleX: 1,
@@ -52,10 +51,28 @@ preloader.on('complete', () => {
                 scale: 0.1,
                 ease: Power2.easeInOut
             }, '-= 1.5')
+            .staggerFrom($dragCursorDot, 0.4, {
+                opacity: 0,
+                scaleX: 0,
+                ease: Expo.easeInOut
+            }, 0,'-= 0.2')
+            .from($dragLine, 1.2, {
+                transformOrigin: 'right',
+                scaleX: 0,
+                ease: Expo.easeInOut
+            })
+            .from($dragCursor, 1.2, {
+                x: $dragLine.offsetWidth,
+                ease: Expo.easeInOut
+            }, '-= 1.2')
+            .to($dragCircle, 1.2, {
+                strokeDashoffset: 0,
+                ease: Expo.easeInOut
+            }, '-= 1.2')
             .set($loader, {
                 display: 'none'
             })
-        }, 3000);
+        }, 1000);
     })
 })
 
